8e47070f6e2752aba529ac65d7460dc1a97c23ab
critechar <john.ch.fr@gmail.com>
2015-03-21 01:34:01 +0530

    Added exponential random number generation.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![feature(alloc)]
 // use of Weak, downgrade, strong_count
 
+extern crate rand;
+
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
+use rand::Rng;
+use rand::distributions::IndependentSample;
+use rand::distributions::exponential::Exp;
 
 #[derive(Debug)]
 struct Request {
@@ -94,18 +99,20 @@ impl PartialOrd for Event {
 // Event }}}
 
 fn main() {
-    use EventType::{Arrival, Departure, Timeout, QuantumOver};
+    use std::num::from_f64;
+    use EventType::{Arrival, Timeout};
     let mut events = BinaryHeap::new();
-    let req = Rc::new(RefCell::new(Request { id: 89, arrival_time: 4, total_service: 4, remaining_service: 4 }));
+    let mut rng = rand::thread_rng();
+    let exp = Exp::new(1.0/8.0);
+    let mut v = exp.ind_sample(&mut rng);
+    println!("rand: {}", v);
+    let req = Rc::new(RefCell::new(Request { id: 89, arrival_time: from_f64::<usize>(v).unwrap(), total_service: 4, remaining_service: 4 }));
     let c = Rc::new(RefCell::new(Core { status: CoreStatus::Idle, request: None, quantum_start: 0, total_busy_time: 0 }));
-    let e = Event { _type: Arrival(req.clone()), timestamp: 4 };
+    let e = Event { _type: Arrival(req.clone()), timestamp: req.borrow().arrival_time };
     events.push(e);
-    let e = Event { _type: Departure(c.clone()), timestamp: 8 };
+    v += exp.ind_sample(&mut rng);
+    let e = Event { _type: Timeout(req.clone().downgrade()), timestamp: from_f64::<usize>(v).unwrap() };
     events.push(e);
-    let e = Event { _type: Timeout(req.clone().downgrade()), timestamp: 8 };
-    events.push(e);
-    let c = events.pop().unwrap();
-    println!("{:?}", &c);
     let c = events.pop().unwrap();
     println!("{:?}", &c);
     let c = events.pop().unwrap();
