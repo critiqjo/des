c78f92daf717bdb47d8cd1dad8ecf8d8dedb21fb
critechar <john.ch.fr@gmail.com>
2015-03-22 01:20:32 +0530

    First major step: Requests ease-in, prior to event-loop

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::collections::BinaryHeap;
 
 extern crate rand;
 use rand::distributions::IndependentSample;
-use rand::distributions::exponential::Exp;
+use rand::distributions::{Exp, Normal, Range};
 
 mod cpu;
 mod event;
@@ -16,25 +16,47 @@ use cpu::{Cpu, CpuState};
 use event::{Event, EventType};
 use request::Request;
 
+const N_CPU: usize = 4;
+const N_USERS: usize = 32;
+const EASE_IN_TIME: f64 = 20.0;
+
+const REQ_SERVICE_TIME_MEAN: f64 = 2.0;
+const REQ_TIMEOUT_MIN: f64 = 10.0;
+const REQ_TIMEOUT_MAX: f64 = 30.0;
+
+#[derive(Debug)]
+struct SystemMetrics {
+    time: f64,
+    sum_resp_time: f64,
+    n_req_proc: usize,
+    n_req_timeo: usize,
+    n_req_drops: usize,
+}
+
 fn main() {
+    let mut sys = SystemMetrics { time: 0.0, sum_resp_time: 0.0, n_req_proc: 0, n_req_timeo: 0, n_req_drops: 0 };
     let mut events = BinaryHeap::new();
     let mut rng = rand::thread_rng();
-    let exp = Exp::new(1.0/8.0);
+    let mut cpus = Vec::with_capacity(N_CPU);
+    for _ in 0..N_CPU-1 {
+        cpus.push(Rc::new(RefCell::new(Cpu::new())));
+    }
+    let cpus = cpus; //remove mutability
+    let ease_in = Range::new(0.0_f64, EASE_IN_TIME);
+    let service = Exp::new(1.0/REQ_SERVICE_TIME_MEAN);
+    let timeout = Range::new(REQ_TIMEOUT_MIN, REQ_TIMEOUT_MAX);
 
-    let mut v = exp.ind_sample(&mut rng);
+    for _ in 0..N_USERS {
+        let req = Rc::new(RefCell::new(Request::new(sys.time, &ease_in, &service, &mut rng)));
+        let arrival = req.borrow().arrival_time;
+        let e = Event { _type: EventType::Arrival(req.clone()), timestamp: arrival };
+        events.push(e);
+        let e = Event { _type: EventType::Timeout(req.clone().downgrade()), timestamp: arrival + timeout.ind_sample(&mut rng) };
+        events.push(e);
+    }
 
-    let req = Rc::new(RefCell::new(Request { id: 89, arrival_time: v, total_service: 4.0, remaining_service: 4.0 }));
-    let mut e = Event { _type: EventType::Arrival(req.clone()), timestamp: req.borrow().arrival_time };
-    events.push(e);
-
-    v += exp.ind_sample(&mut rng);
-    e = Event { _type: EventType::Timeout(req.clone().downgrade()), timestamp: v };
-    events.push(e);
-
-    let mut e = events.pop().unwrap();
-    println!("{:?}", &e);
-    e = events.pop().unwrap();
-    println!("{:?}", &e);
-    assert_eq!(events.pop(), None);
+    while let Some(e) = events.pop() {
+        println!("{:?}", &e);
+    }
 }
 
--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,6 @@
+use rand::ThreadRng;
+use rand::distributions::IndependentSample;
+
 #[derive(Debug)]
 pub struct Request {
     pub id: usize,
@@ -5,3 +8,15 @@ pub struct Request {
     pub total_service: f64,
     pub remaining_service: f64,
 }
+
+static mut next_id:usize = 0;
+impl Request {
+    pub fn new<A: IndependentSample<f64>, S: IndependentSample<f64>>
+        (now: f64, arrival_sampler: &A, service_sampler: &S, rng: &mut ThreadRng) -> Request {
+        unsafe { next_id += 1; }
+        let arrival = now + arrival_sampler.ind_sample(rng);
+        let service = service_sampler.ind_sample(rng);
+        Request { id: unsafe { next_id }, arrival_time: arrival, total_service: service, remaining_service: service }
+    }
+}
+
