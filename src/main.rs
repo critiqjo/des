77275f0347ba074dfef1769d0a5a4b9af4fa8aa1
critechar <john.ch.fr@gmail.com>
2015-03-24 20:42:37 +0530

    JSON formatted output

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,17 @@ use std::io::{self, Read};
 use std::borrow::Borrow;
 use rustc_serialize::json;
 
+#[derive(RustcDecodable, RustcEncodable)]
+struct SimResult {
+    arrival_rate: f64,
+    throughput: f64,
+    goodput: f64,
+    resp_time: f64,
+    cpu_util: f64,
+    reqs_in_sys: f64,
+    failed_frac: f64,
+}
+
 fn main() {
     let mut stdin = io::stdin();
     let mut sys_json = String::new();
@@ -22,20 +33,15 @@ fn main() {
         Err(e) => panic!("Fatal error: {:?}", e),
     };
     let sim = simulation::run(&sys);
-    let avg_arrival_rate = sim.n_arrivals as f64 / sim.time;
-    let tput = sim.n_processed as f64/sim.time;
     let n_processed_to = sim.n_timedout - sim.n_to_in_proc;
-    let gput = (sim.n_processed - n_processed_to) as f64/sim.time;
-    let avg_resp_time = sim.sum_resp_time/sim.n_processed as f64;
-    let avg_cpu_util = sim.total_cpu_time / sim.time / sys.n_cpu as f64;
-    let avg_reqs_in_sys = sim.wt_sum_reqs_in_sys / sim.total_cpu_time;
-    let ffrac = (sim.n_dropped + n_processed_to) as f64/(sim.n_dropped + sim.n_processed) as f64;
-    println!("
-  Avg arrival rate = {}
-  Avg throughput = {}
-  Avg goodput = {}
-  Avg response time = {}
-  Avg CPU utilization = {}
-  Avg number of requests in system = {}
-  Fraction of failed requests = {}\n", avg_arrival_rate, tput, gput, avg_resp_time, avg_cpu_util, avg_reqs_in_sys, ffrac);
+    let sim_res = SimResult {
+        arrival_rate: sim.n_arrivals as f64 / sim.time,
+        throughput: sim.n_processed as f64/sim.time,
+        goodput: (sim.n_processed - n_processed_to) as f64/sim.time,
+        resp_time: sim.sum_resp_time/sim.n_processed as f64,
+        cpu_util: sim.total_cpu_time / sim.time / sys.n_cpu as f64,
+        reqs_in_sys: sim.wt_sum_reqs_in_sys / sim.total_cpu_time,
+        failed_frac: (sim.n_dropped + n_processed_to) as f64/(sim.n_dropped + sim.n_processed) as f64,
+    };
+    println!("{}", json::as_pretty_json(&sim_res));
 }
