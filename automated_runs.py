0f0591f2ccf5089f9f289142b221ebe03427e624
samkit <samkit993@gmail.com>
2015-04-03 05:50:07 +0530

    Small fixes

--- automated_runs.py
+++ automated_runs.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 from math import sqrt
 from scipy import stats
 
-def plot(xs, ys, xtitle, ytitle,main_title, file_name, scatter=False):
+def plot(xs, ys, xtitle, ytitle,main_title, file_name, scatter=False, errorbar=False, error_rate=0.0, intervals=[]):
     fig = plt.figure()
     if scatter:
         plt.scatter(xs,ys)
+        if errorbar:
+            plt.errorbar(xs, ys, yerr=intervals, linestyle="None")
     else:
         plt.plot(xs, ys)
     plt.ylim([0,max(ys)*1.2])
@@ -33,6 +35,16 @@ def conf_ivals(samples, error):
     alpha = stats.norm.ppf(1 - error/2)
     return (m - sd*alpha/sqrt(n), m + sd*alpha/sqrt(n))
 
+def compute_ci(sys, variable, error_rate, iters):
+    vec = []
+    sys[variable] = 40
+    for i in range(1, iters+1):
+        sim_run = subprocess.Popen(['target/debug/des'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        res = json.loads(sim_run.communicate(json.dumps(sys))[0])
+        vec.append(res[variable])
+    resp_lb, resp_ub = conf_ivals(vec, error_rate)
+    return resp_lb, mean(vec), resp_ub
+
 
 sys = dict()
 multi_run_file = open("auto_runs.json", "r")
@@ -47,16 +59,6 @@ del sys["n_users_end"]
 cargo = subprocess.Popen(['cargo', 'build'])
 cargo.wait()
 
-#Simulation for Computing Condidence Interval for Reponse Time with Error Rate 5%
-error_rate = 0.05
-r_times = []
-sys["n_users"] = 40
-for i in range(1, 30):
-    sim_run = subprocess.Popen(['target/debug/des'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    res = json.loads(sim_run.communicate(json.dumps(sys))[0])
-    r_times.append(res["resp_time"])
-resp_lb, resp_ub = conf_ivals(r_times, error_rate)
-print "\n95% Confidence Interval of Response Time: ", resp_lb, resp_ub
 
 #Simulation with varied number of users
 n_users = []
@@ -68,7 +70,8 @@ tputs = []
 tfracs = []
 dfracs = []
 drates = []
-
+ivals = []
+error_rate = 0.05
 
 for x in range(n_users_start, n_users_end, 2):
     n_users.append(x)
@@ -80,9 +83,8 @@ for x in range(n_users_start, n_users_end, 2):
     temp_dfrac = 0.0
     temp_drate = 0.0
     temp_resp_time = 0.0
-
+    sys["n_users"] = x
     for i in range(0,3):
-        sys["n_users"] = x
         sim_run = subprocess.Popen(['target/debug/des'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         res = json.loads(sim_run.communicate(json.dumps(sys))[0])
         temp_tput += res["throughput"]
@@ -93,6 +95,10 @@ for x in range(n_users_start, n_users_end, 2):
         temp_dfrac += res["dropped_frac"]
         temp_drate += res["drop_rate"]
         temp_resp_time += res["resp_time"]
+
+    (resp_lb, resp_mean, resp_ub) = compute_ci(sys, "resp_time", error_rate, 30)
+    ivals.append((resp_ub-resp_lb)/2)
+    print "Average response time ",resp_mean, " lies with ", (1-error_rate)*100,"% confidence in interval (", resp_lb, ",", resp_ub, ")"
     tputs.append(temp_tput/3.0)
     gputs.append(temp_gput/3.0)
     bputs.append(temp_bput/3.0)
@@ -100,13 +106,14 @@ for x in range(n_users_start, n_users_end, 2):
     tfracs.append(temp_tfrac/3.0)
     dfracs.append(temp_dfrac/3.0)
     drates.append(temp_drate/3.0)
-    resp_times.append(temp_resp_time/3.0)
+    resp_times.append(resp_mean)
 
-plot(n_users, resp_times, "Number of Users", "Response Time", "Number of Users Vs. Response Times", "nusers_v_resp.png");
+plot(n_users, resp_times, "Number of Users", "Response Time", "Response Times Vs. Number of Users ", "nusers_v_resp.png",True, True, error_rate, ivals)
+exit
 plot(n_users, gputs, "Number of Users", "Goodput",  "Goodput Vs. Number of Users", "gput_v_nusers.png");
 plot(n_users, bputs, "Number of Users", "Badput",  "Badput Vs. Number of Users", "bput_v_nusers.png");
 plot(n_users, tputs, "Number of Users", "Throughput",  "Througput Vs. Number of Users ", "tput_v_nusers.png");
-plot(n_users, utils, "Number of Users", "Server CPU Utilization", "CPU Utilization Vs. Number of Users", "util_v_util.png");
+plot(n_users, utils, "Number of Users", "Server CPU Utilization", "CPU Utilization Vs. Number of Users", "util_v_nusers.png");
 plot(tputs, resp_times, "Throughput", "Response Time", "Response Time Vs. Throughput", "resp_v_tput.png", True);
 plot(tputs, utils, "Throughput", "Server CPU Utilization", "CPU Utilization Vs. Throughput", "util_v_tput.png");
 plot(tputs, utils, "Throughput", "Server CPU Utilization", "CPU Utilization Vs. Throughput", "util_v_tput.png", True);
